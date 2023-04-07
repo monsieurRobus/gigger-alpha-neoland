0d9c7ea9be73690aa369abebc169549d89c88af2
Carlos Díaz <cdperez.85@gmail.com>
2023-04-07 14:10:00 +0200

    Fix in styles and css vars

--- gigger-app/src/ui/StyledButton/StyledButton.jsx
+++ gigger-app/src/ui/StyledButton/StyledButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-import { COLOR_PRIMARY, COLOR_LIGHT } from '../../styles/Variables'
 
 const StyledButton = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: ${({logout})=>logout && '#e1e1e1'};
+    background-color: ${({logout})=>logout && 'var(--color-medium)'};
     border: 1px solid #f5f5f5;
     border-radius: 5px;
     color: #000;
@@ -19,9 +18,9 @@ const StyledButton = styled.button`
     box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px;
 
     &:hover {
-        background-color: ${COLOR_PRIMARY};
+        background-color: var(--color-primary);
         border: 1px solid #000;
-        color: ${COLOR_LIGHT};
+        color: var(--color-light);
     }
 `
 
